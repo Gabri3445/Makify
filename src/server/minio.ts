17f34c14cfe9806921aa04af2639eae41ce09711
Gabriele Lopez <gabriele.ope@gmail.com>
2024-02-20 22:46:35 +0100

    added logic for deployment

--- src/server/minio.ts
+++ src/server/minio.ts
@@ -2,17 +2,23 @@ import * as Minio from 'minio'
 
 import { env } from "~/env";
 
-const globalForPrisma = globalThis as unknown as {
+const globalForMinio = globalThis as unknown as {
   minio: Minio.Client | undefined;
 };
+
 export const minio =
-  globalForPrisma.minio ??
+  globalForMinio.minio ??
   new Minio.Client({
-    endPoint:env.MINIO_ENDPOINT,
+    endPoint: env.MINIO_ENDPOINT,
     port: env.MINIO_PORT,
     useSSL: env.MINIO_USE_SSL === "true", // dumb but works
     accessKey: env.MINIO_ACCESS_KEY,
     secretKey: env.MINIO_SECRET_KEY,
   })
 
-if (env.NODE_ENV !== "production") globalForPrisma.minio = minio
+const exists = await minio.bucketExists(env.MINIO_BUCKET)
+if (!exists) {
+  await minio.makeBucket(env.MINIO_BUCKET)
+}
+
+if (env.NODE_ENV !== "production") globalForMinio.minio = minio
