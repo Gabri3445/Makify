85648214ff10a239e2a73f95448ca86dd6ce5003
Gabriele Lopez <gabriele.ope@gmail.com>
2024-02-20 02:36:34 +0100

    removed test stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-import MySignIn from "~/components/MySignIn"
-import { getServerAuthSession } from "~/server/auth";
-import { db } from "~/server/db";
-import { minio } from "~/server/minio";
-
 export default async function HomePage() {
 
-  const session = await getServerAuthSession()
+  /*const session = await getServerAuthSession()
 
   const user = await db.user.findUnique({
     where: {
@@ -14,11 +9,11 @@ export default async function HomePage() {
   })
 
 
-  const post = await db.post.findFirst()
+  const post = await db.post.findFirst()*/
 
   return (
       <div>
-        <MySignIn signedIn={session ? true : false} username={session?.user.name} posts={post?.name}/>
+        
       </div>
   );
 }
