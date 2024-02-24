b1603e080571b1210daae73020daa964b37464a9
Gabriele Lopez <gabriele.ope@gmail.com>
2024-02-24 18:19:32 +0100

    cleaner way of generating the mock items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,15 +13,14 @@ export default async function HomePage() {
 
   const post = await db.post.findFirst()*/
 
+  const array = new Array(20).fill(0)
+
   return (
       <div className="m-5">
         <div className="flex flex-wrap justify-center">
-        <ItemCard userLink="fdsfds" itemLink="dds" image="https://images.pexels.com/photos/20241941/pexels-photo-20241941/free-photo-of-cute-cats-near-building.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" likes={0} name="Benchy" userImage="https://cdn-icons-png.flaticon.com/512/2815/2815428.png" username="Gabri3445"></ItemCard>
-        <ItemCard userLink="fdsfds" itemLink="dds" image="https://images.pexels.com/photos/20241941/pexels-photo-20241941/free-photo-of-cute-cats-near-building.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" likes={0} name="Benchy" userImage="https://cdn-icons-png.flaticon.com/512/2815/2815428.png" username="Gabri3445"></ItemCard>
-        <ItemCard userLink="fdsfds" itemLink="dds" image="https://images.pexels.com/photos/20241941/pexels-photo-20241941/free-photo-of-cute-cats-near-building.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" likes={0} name="Benchy" userImage="https://cdn-icons-png.flaticon.com/512/2815/2815428.png" username="Gabri3445"></ItemCard>
-        <ItemCard userLink="fdsfds" itemLink="dds" image="https://images.pexels.com/photos/20241941/pexels-photo-20241941/free-photo-of-cute-cats-near-building.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" likes={0} name="Benchy" userImage="https://cdn-icons-png.flaticon.com/512/2815/2815428.png" username="Gabri3445"></ItemCard>
-        <ItemCard userLink="fdsfds" itemLink="dds" image="https://images.pexels.com/photos/20241941/pexels-photo-20241941/free-photo-of-cute-cats-near-building.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" likes={0} name="Benchy" userImage="https://cdn-icons-png.flaticon.com/512/2815/2815428.png" username="Gabri3445"></ItemCard>
-        <ItemCard userLink="fdsfds" itemLink="dds" image="https://images.pexels.com/photos/20241941/pexels-photo-20241941/free-photo-of-cute-cats-near-building.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" likes={0} name="Benchy" userImage="https://cdn-icons-png.flaticon.com/512/2815/2815428.png" username="Gabri3445"></ItemCard>
+        {array.map((_, i) => (
+            <ItemCard key={i} userLink="fdsfds" itemLink="dds" image="https://images.pexels.com/photos/20241941/pexels-photo-20241941/free-photo-of-cute-cats-near-building.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" likes={0} name="Benchy" userImage="https://cdn-icons-png.flaticon.com/512/2815/2815428.png" username="Gabri3445"></ItemCard>
+        ))}
         </div>
       </div>
   );
