07484010ab51f8faf7114780fc9395f481273dba
Gabriele Lopez <gabriele.ope@gmail.com>
2024-02-29 01:45:04 +0100

    changed props to test for text overflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import ItemCard from "~/components/ItemCard/ItemCard";
+import { getServerAuthSession } from "~/server/auth";
 
 export default async function HomePage() {
 
-  /*const session = await getServerAuthSession()
+  //const session = await getServerAuthSession()
 
+  /*
   const user = await db.user.findUnique({
     where: {
       id: session?.user.id,
@@ -19,7 +21,7 @@ export default async function HomePage() {
       <div className="m-5">
         <div className="flex flex-wrap justify-center">
         {array.map((_, i) => (
-            <ItemCard key={i} userLink="fdsfds" itemLink="dds" image="https://images.pexels.com/photos/20241941/pexels-photo-20241941/free-photo-of-cute-cats-near-building.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" likes={0} name="Benchy" userImage="https://cdn-icons-png.flaticon.com/512/2815/2815428.png" username="Gabri3445"></ItemCard>
+            <ItemCard key={i} userLink="fdsfds" itemLink="dds" image="https://images.pexels.com/photos/20241941/pexels-photo-20241941/free-photo-of-cute-cats-near-building.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" likes={0} name="Articulated flexy Nicolo Martiradonna" userImage="https://cdn-icons-png.flaticon.com/512/2815/2815428.png" username="Gabri3445"></ItemCard>
         ))}
         </div>
       </div>
