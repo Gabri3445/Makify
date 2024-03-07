d98e44a89a6334f00538f1a696d516e9326a9ba8
Gabriele Lopez <gabriele.ope@gmail.com>
2024-03-07 20:54:52 +0100

    rounded the username input

--- src/components/EditProfile/EditUsername.tsx
+++ src/components/EditProfile/EditUsername.tsx
@@ -24,7 +24,7 @@ const EditUsername = (props: EditUsernameProps) => {
         <form className="mt-5 flex justify-between" action={changeUsername}>
             <div className="flex items-center">
                 <label className="mr-3" htmlFor="username">Change your username (Please refresh the page to see the changes): </label>
-                <input required className="bg-gray-500 caret-white" type="text" id="username" name="username"></input>
+                <input required className="bg-gray-500 caret-white rounded-md border border-white" type="text" id="username" name="username"></input>
             </div>
             <input className="bg-blue-500 p-3 rounded-md cursor-pointer" type="submit"></input>
         </form>
